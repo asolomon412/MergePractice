36c89ef8ab0f4b01fbfdf5539ccfca11535e469a
mtedder <maurice_tedder@hotmail.com>
2018-02-07 11:30:55 -0500

    initial commit

---
+++ src/MergePractice.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author maurice tedder
+ *
+ */
+public class MergePractice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is moe");
+	}
+
+}
